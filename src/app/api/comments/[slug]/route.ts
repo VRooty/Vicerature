3502b103215dd3f0b3e390bf3b3aee015cdb2202
Vrooty <victoriadohertybone@live.co.uk>
2023-11-28 13:53:23 +0000

    message

---
+++ src/app/api/comments/[slug]/route.ts
@@ -0,0 +1,23 @@
+import { getComments, saveComment } from "@/lib/comments";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest, {params} : {params: {slug: string}}) {
+
+        const slug =params.slug;
+        const comments = await getComments(slug) 
+        return NextResponse.json({comments})   
+}
+
+export async function POST(request: NextRequest, {params}: {params: {slug: string}}) {
+    const slug = params.slug
+
+    const formData= await request.formData()
+    const userName= formData.get('username') as string;
+    const comment= formData.get('comment') as string;
+
+    await saveComment(userName, comment, slug)
+
+    return NextResponse.json('comment saved!')
+
+}
