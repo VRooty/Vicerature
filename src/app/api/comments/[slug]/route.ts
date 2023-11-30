320506dd6ddd69d0173499d5e27660b1bd353487
Vrooty <victoriadohertybone@live.co.uk>
2023-11-30 11:44:56 +0000

    init

--- src/app/api/comments/[slug]/route.ts
+++ src/app/api/comments/[slug]/route.ts
@@ -1,23 +1,22 @@
 import { getComments, saveComment } from "@/lib/comments";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
 
 export async function GET(request: NextRequest, {params} : {params: {slug: string}}) {
-
-        const slug =params.slug;
-        const comments = await getComments(slug) 
-        return NextResponse.json({comments})   
+ 
+    try {
+        const slug = params.slug;
+        const comments = await getComments(slug)
+        return NextResponse.json({comments})
+    } catch (error) {
+        return NextResponse.json({error})
+    }
 }
 
-export async function POST(request: NextRequest, {params}: {params: {slug: string}}) {
+export async function POST(request: NextRequest, {params} : {params: {slug: string}}) {
     const slug = params.slug
 
-    const formData= await request.formData()
-    const userName= formData.get('username') as string;
-    const comment= formData.get('comment') as string;
-
-    await saveComment(userName, comment, slug)
-
-    return NextResponse.json('comment saved!')
-
+    const formData = await request.formData()
+    const username = formData.get('username') as string;
+    const comment = formData.get('comment') as string;
 }
---
+++ src/app/components/CommentForm.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { WEB_SITE } from "config";
+import FormStatusButton from "./FormStatusButton";
+import { saveComment } from "@/lib/comments";
+import { revalidatePath } from "next/cache";
+
+
+
+export function CommentForm({ postSlug }: {postSlug: string}) {
+
+    console.log('This is running on the browser')
+
+
+
+    async function handleFormSubmit(formData: FormData) {
+      'use server'
+      console.log("submiting the form");
+  
+      const username = formData.get('username') as string
+      const comment = formData.get('comment') as string
+      await saveComment(username, comment, postSlug)
+  
+      
+    }
+  
+    return (
+      <form action={handleFormSubmit}>
+        <label htmlFor="username">Name</label>
+        <input type="text" name="username" className='text-neutral-900'/>
+        <label htmlFor="comment">Comment</label>
+        <textarea name="comment" cols={30} rows={10} className='text-neutral-900'/>
+        <FormStatusButton></FormStatusButton>
+      </form>
+    );
+  }
+
+   
