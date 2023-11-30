37db1563c5ab46dab57f6cff20e8764b6253357e
Vrooty <victoriadohertybone@live.co.uk>
2023-11-30 11:59:56 +0000

    ts ignore

--- src/app/components/Comments.tsx
+++ src/app/components/Comments.tsx
@@ -1,12 +1,12 @@
-export default async function Comments({postSlug}: {postSlug:string}) {
+import { WEB_SITE } from "config";
 
-    const WEBSITE_URL = 'http://localhost:3000'
+export default async function Comments({postSlug}: {postSlug:string}) {
 
 
 let comments = [];
 
 try {
-    const commentsResult = await fetch(`${WEBSITE_URL}/api/comments/${postSlug}`, {next: {revalidate: 5}})
+    const commentsResult = await fetch(`${WEB_SITE}/api/comments/${postSlug}`, {next: {revalidate: 0}})
     const response = await commentsResult.json()
     console.log(response)
     comments = response.comments.rows
